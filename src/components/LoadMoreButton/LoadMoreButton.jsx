e2b3e566fc7ac195de87510f5aa255e8838d2254
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-08-11 22:17:45 +0200

    add scroll

--- src/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,15 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getCamperList } from '@redux/campers/operations';
 import { Button } from 'shared';
+import { selectParams } from '@redux/campers/selectors';
 import css from './LoadMoreButton.module.css';
-import { selectParams } from '../../redux/campers/selectors';
 
 const LoadMoreButton = () => {
   const dispatch = useDispatch();
   const params = useSelector(selectParams);
 
   const handleLoadMore = () => {
-    console.log(params);
     dispatch(getCamperList(params));
   };
 
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -9,17 +9,27 @@ import { getInitialCamperList } from '@redux/campers/operations';
 import { LoadMoreButton, Loader, CampersList, FilterBar } from 'components';
 import { Container } from 'shared';
 import css from './Pages.module.css';
+import { selectPage } from '../redux/campers/selectors';
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
   const hasNextPage = useSelector(selectHasNextPage);
   const camperList = useSelector(selectCamperList);
+  const page = useSelector(selectPage);
 
   useEffect(() => {
     if (camperList.length) return;
     dispatch(getInitialCamperList({ page: 1, limit: 4, form: '' }));
   }, [dispatch, camperList]);
+
+  useEffect(() => {
+    if (page <= 2) return;
+    const timer = setTimeout(() => {
+      window.scrollBy({ top: 500, behavior: 'smooth' });
+    }, 0);
+    return () => clearTimeout(timer);
+  }, [page]);
 
   return (
     <Container className={css.catalogPageContainer}>
