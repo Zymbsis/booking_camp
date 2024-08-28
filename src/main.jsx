38dedd833708b3ffa159be7e478d64c59e1a71c8
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-08-28 12:10:33 +0200

    redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { ModalProvider } from 'components';
+import ModalProvider from './components/ModalWindow/ModalProvider/ModalProvider.jsx';
+
 import App from './App.jsx';
 import './styles/index.css';
 
